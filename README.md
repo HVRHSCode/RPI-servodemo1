838e43ed9ee8f4a3053edc7d17947ad7afcc2779
cannotilever <cannotilever@protonmail.com>
2020-09-13 12:42:55 -0400

    add bounce program and corresponding requirements

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RPI-servodemo1
+
+This is a simple demonstration of Servo control on Raspberry Pi with python.
+
+### REMEMBER TO ENABLE I2C
+
+Documentation of library: https://learn.adafruit.com/adafruit-16-channel-pwm-servo-hat-for-raspberry-pi/using-the-python-library
---
+++ bounce.py
@@ -0,0 +1,22 @@
+import time
+from adafruit_servokit import ServoKit
+hat = ServoKit(channels=16)
+angle = 0
+direction = -1
+def get_servos():
+    servos = input("How many servos are connected? (type a whole number) ")
+    try: 
+        servos = int(servos)
+    except(ValueError): 
+        print("Invalid input.\n")
+        get_servos()
+    return servos-1
+servos = get_servos()
+while True:
+    print("Setting angle {}".format(angle))
+    for i in range (servos):
+        hat.servo[i].angle = angle
+    if (angle >= 180) or (angle <= 0):
+        direction = -direction
+    angle += (10*direction)
+    time.sleep(0.6)
